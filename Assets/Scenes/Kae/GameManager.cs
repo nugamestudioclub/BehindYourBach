984c62eb5d19849189e8b4bbf0717aa7f2f312ea
K3nn8DN <kendrajanel04@gmail.com>
2023-10-20 22:44:29 -0400

    update

---
+++ Assets/Scenes/Kae/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+
+    int enemyCount;
+    int level;
+
+    private void Start()
+    {
+        enemyCount = 0;
+        level = 1;
+    }
+
+
+    public void openDoor()
+    {
+        
+    }
+
+    public void startRoom()
+    {
+        //takes in level or which room we're in
+        //close doors, spawn enemy, set count, despawns last room, spawns next room
+    }
+}
--- Assets/Scenes/Kae/Player.cs
+++ Assets/Scenes/Kae/Player.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //aim,shoot,move
+    int lives;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        lives = 3;
     }
 }
